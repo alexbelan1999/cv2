d6db344f02091227f6ec4619744d776713f82bbe
alexbelan1999 <alexbelan1999@gmail.com>
2019-12-14 22:43:13 +0300

    Add exportdb

--- importdb.py
+++ importdb.py
@@ -6,24 +6,24 @@ exit = True
 connection = None
 
 try:
-    print("Загрузка данных на сервер.")
-    n = int(input("Введите 1 для загрузки данных с веб-камеры, 2 для загрузки данных с фотографий: "))
+    print("Скачивание данных с сервер.")
+    n = int(input("Введите 1 для скачивание данных для веб-камеры, 2 для скачивание данных для фотографий: "))
     sql = " "
     if n == 1:
-        sql = "INSERT INTO public.datacv2 (name, object) VALUES ('web_camera_data', lo_import('" + os.getcwd() + r'\face_recognition\face_recognition_web_camera.yml' + "'));"
+        sql = "SELECT lo_export(object,'" + os.getcwd() + r"\face_recognition\face_recognition_web_camera1.yml') from datacv2 where name = 'web_camera_data';"
 
     elif n == 2:
-        sql = "INSERT INTO public.datacv2 (name, object) VALUES ('photo_data', lo_import('" + os.getcwd() + r'\face_recognition\face_recognition_photo.yml' + "'));"
+        sql = "SELECT lo_export(object,'" + os.getcwd() + r"\face_recognition\face_recognition_photo1.yml') from datacv2 where name = 'photo_data';"
 
     else:
         print("Ошибка выбора варианта!")
 
     connection = psycopg2.connect(dbname='cv2', user='postgres', password='1234', host='127.0.0.1')
     with connection.cursor() as cursor:
-
         cursor.execute(sql)
         connection.commit()
         cursor.close()
+
 
 except psycopg2.OperationalError:
     print("Ошибка соединения с базой данных!")
@@ -32,4 +32,4 @@ except psycopg2.OperationalError:
 finally:
     if exit == True:
         connection.close()
-        print("Отправка файлов на сервер завершена. Соединение закрыто")
+        print("Скачивание файлов с сервера завершено. Соединение закрыто")
